a16e3dcbe8fc5234f9bcdc6b8b358555915d1527
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-06-20 20:27:20 -0400

    Search titles updated.

--- www/javascript/app.js
+++ www/javascript/app.js
@@ -3,9 +3,9 @@ jQuery( document ).ready(function( $ ) {
 		$('input[name="search"]').on('keyup', function(evt){
 
 			$('article p:not(:contains("'+$(this).val()+'"))').parent().fadeOut()
-			$('article a:not(:contains("'+$(this).val()+'"))').parent().fadeOut()
+			$('article h3:not(:contains("'+$(this).val()+'"))').parent().fadeOut()
 			$('article p:contains("'+$(this).val()+'")').parent().fadeIn()
-			$('article a:contains("'+$(this).val()+'")').parent().fadeIn()
+			$('article h3:contains("'+$(this).val()+'")').parent().fadeIn()
 
 			if($(this).val().length <= 0){
 				$('article').fadeIn()		
