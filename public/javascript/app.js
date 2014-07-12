f6041702c83bba77de0d1123d04300a41ba3e8f1
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-07-12 11:10:14 -0400

    Stopped blinking effect.
    Thanks
    http://stackoverflow.com/questions/10114835/prevent-another-event-from-firing-before-a-previous-one-is-complete-in-jquery

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,3 +1,18 @@
+jQuery.fn.fadeOut = function (duration, callback) {
+    jQuery(this).stop().fadeTo(duration, 0, function () {
+        jQuery(this).css('display', 'none');
+        if (typeof callback == 'function') {
+            callback();
+        }
+    });
+};
+jQuery.fn.fadeIn = function (duration, callback) {
+    jQuery(this).css('display', 'inline-block').stop().fadeTo(duration, 1, function () {
+        if (typeof callback == 'function') {
+            callback();
+        }
+    });
+};
 jQuery( document ).ready(function( $ ) {
 	if( $('input[name="search"]').length > 0 ){
 		$('input[name="search"]').on('keyup', function(evt){
