cd274cb083b8481b3898d2e6f18dee8f3489bf15
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-10-09 18:29:34 -0400

    Compiled Harp

--- www/javascript/app.js
+++ www/javascript/app.js
@@ -17,37 +17,8 @@ jQuery( document ).ready(function( $ ) {
 	if( $('input[name="search"]').length > 0 ){
 		$('input[name="search"]').on('keyup', function(evt){
 
-			var search = $(this).val().toUpperCase()
-
-			$('article').contents().filter(function() {
-				if( this.innerHTML ){
-					if(this.innerHTML.toUpperCase().indexOf(search) >= 0){
-						return false;
-					}
-				}
-      			return true
-    		}).closest('article').fadeOut()
-
-    		$('article').contents().filter(function() {
-				if( this.innerHTML ){
-					if(this.innerHTML.toUpperCase().indexOf(search) >= 0){
-						return true;
-					}
-				}
-      			return false
-    		}).closest('article').fadeIn()
-
-			if($(this).val().length <= 0){
-				$('article').fadeIn()		
-			}
-
-			evt.stopPropagation()
-		})
-
-		$('input[name="search"]').on('blur', function(evt){
-			if($(this).val().length <= 0){
-				$('article').fadeIn()		
-			}
+			var search = $(this).val().toLowerCase()
+			d3Filters(search)
 		})
 	}
 })
