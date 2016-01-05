20aee7bfa179788e8c01c05922ece48a6df045f2
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-01-05 01:03:51 -0500

    SRI Encoding for CSS links is working
    Wow, beautiful soup is really quite pleasant to work with.

--- sri.py
+++ sri.py
@@ -19,12 +19,57 @@ def sha384OfFile(filepath):
 
 def sriOfFile(filepath):
 	sha = sha384OfFile(filepath)
-	return encodestring(sha)
+	return encodestring(sha)[:-1] #drop newline added by b64
+
+import os
+
+def get_filepaths(directory):
+    """
+    This function will generate the file names in a directory 
+    tree by walking the tree either top-down or bottom-up. For each 
+    directory in the tree rooted at directory top (including top itself), 
+    it yields a 3-tuple (dirpath, dirnames, filenames).
+    """
+    file_paths = []  # List which will store all of the full filepaths.
+
+    # Walk the tree.
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # Join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)  # Add it to the list.
+
+    return file_paths  # Self-explanatory.
+
+searchLinks = []
+searchScripts = []
 
 for linkArray in conf['links']:
 	sri = sriOfFile(linkArray[0])
-	print sri
+	searchLinks.append( [linkArray[1] , sri])
 
 for scriptArray in conf['scripts']:
 	sri = sriOfFile(scriptArray[0])
-	print sri
+	searchScripts.append( [scriptArray[1] , sri])
+
+print searchLinks
+print searchScripts
+
+def isCssLink(tag):
+	return tag.has_attr('rel') and 'stylesheet' in tag['rel']
+
+allFiles = get_filepaths("www/")
+for f in allFiles:
+	if f.endswith(".html"):
+		# Now open it up and update the SRI if neccesary
+		data = ''
+		with open(f,'r') as openedFile:
+			soup = BeautifulSoup(openedFile, "lxml")
+			links = soup.find_all(isCssLink)
+			for link in links:
+				for searchLink in searchLinks:
+					if link['href'].endswith(searchLink[0]):
+						link['integrity'] = searchLink[1]
+			data = soup.prettify()
+		with open(f, 'w') as openedFile:
+			openedFile.write(data.encode('utf-8'))
