56590f5335b8013c3c29b5deb01dc715437ee0f1
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-01-05 00:28:40 -0500

    Updated python with sri computation code
    Able to compute the integrity value for the hash now. Now all I need to
    do is use beautiful soup to play with the files.

--- sri.py
+++ sri.py
@@ -1,12 +1,30 @@
 from bs4 import BeautifulSoup
+from hashlib import sha384
+from base64 import encodestring
 
 import json
-
 with open('sri.conf') as f:
 	conf = json.load(f)
 
-for link in conf['links']:
-	print link
+#http://stackoverflow.com/a/19711609/1808164
+def sha384OfFile(filepath):
+    import hashlib
+    sha = hashlib.sha384()
+    with open(filepath, 'rb') as f:
+        while True:
+            block = f.read(2**10) # Magic number: one-megabyte blocks.
+            if not block: break
+            sha.update(block)
+        return sha.digest() #modified from original so we return binary for encoding
 
-for script in conf['scripts']:
-	print script
+def sriOfFile(filepath):
+	sha = sha384OfFile(filepath)
+	return encodestring(sha)
+
+for linkArray in conf['links']:
+	sri = sriOfFile(linkArray[0])
+	print sri
+
+for scriptArray in conf['scripts']:
+	sri = sriOfFile(scriptArray[0])
+	print sri
